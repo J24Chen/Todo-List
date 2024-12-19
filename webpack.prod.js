49e46a5ed1d769d61aa602e89b520abe752defa9
J24Chen <Jchen2408@gmail.com>
2024-12-19 15:37:41 -0500

    fix error in webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,40 +1,51 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
 
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },    
+    /* 
+    Standard module.exports arrgs. 
+    mode: development | production
+    entry: js file path for entry point (initial file that adds everything together)
+    output: dist destination.
+    */
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist" ),
+        clean:true,
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
+    // Used for devTool and webpack-dev-serve 
+    /* Used for legible error line messages and 
+        to allow live website updates on any code changes 
+    */
+    devtool: "eval-source-map", 
+    devServer: {
+        watchFiles: ["./src/template.html"]
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-
-
+    //Used for html-webpack-plugin
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
     ],
-  },
+    
+    // Specifies rules on how to load .css files, .html files, etc.
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
 
+            {
+                test: /\.html/i,
+                loader: "html-loader",
+            },  
+        
+        ],
+    },
 };
